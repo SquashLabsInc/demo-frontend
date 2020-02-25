097fa1f3d98f2ce007f6a7e1fcbd0540d93f9753
Anna Pershyna <ny4koo@gmail.com>
2020-02-25 01:44:27 +0100

    Update launch_tests.sh

--- launch_tests.sh
+++ launch_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source ~/.bashrc
-PAGE=https://${SQUASH_DOMAIN}/ envsubst < test.template.js > test.js
-xvfb-run testcafe firefox,chrome test.js
+PAGE=https://${SQUASH_DOMAIN} envsubst < test.template.js > test.js
+xvfb-run testcafe --skip-js-errors firefox test.js
+xvfb-run testcafe --skip-js-errors chrome test.js
